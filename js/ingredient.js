9df42aeffad4529874fdc54462de2ad00771ee67
viorn <viorn89@gmail.com>
2019-04-06 11:12:49 +0300

    buttons was added

--- js/ingredient.js
+++ js/ingredient.js
@@ -1,21 +1,24 @@
-
 var ingredientsTable = new Vue({
-	el: "#ingredientsTable",
-	data: {
-     ingredients: []
-    },
-    methods: {
-    	refreshList: function () {
-      		loadAll()
-    	}
+  el: "#ingredientsTable",
+  data: {
+    ingredients: []
+  }
+})
+
+var toolsPanel = new Vue({
+  el: "#toolsPanel",
+  methods: {
+    refreshList: function() {
+      loadAll()
     }
+  }
 })
 
 function loadAll() {
-	axios.get('http://127.0.0.1:8080/ingredient')
-  .then(function (response) {
-  	ingredientsTable.ingredients = response.data.list;
-  });
+  axios.get('http://127.0.0.1:8080/ingredient')
+    .then(function(response) {
+      ingredientsTable.ingredients = response.data.list;
+    });
 }
 
 loadAll();
